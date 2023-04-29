5383ebe6705b7f0d5cacf2861ea6eefc6f1dd61e
Tauseef Ashiq <say2ahmed@hotmail.com>
2023-04-29 01:29:49 +0100

    Adding first commit AOS code

---
+++ 3_Tier_AWS_Network_VPC/jupiter-website-ecs/backend.tf
@@ -0,0 +1,9 @@
+# store the terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket    = "tk-tfstate-remote-backend-dev-account001"
+    key       = "jupiter-website-ecs.tfstate"
+    region    = "eu-west-2"
+    #profile   = default
+  }
+}
---
+++ 3_Tier_AWS_Network_VPC/jupiter-website-ecs/main.tf
@@ -0,0 +1,34 @@
+# provider
+provider "aws" {
+  region = var.region
+}
+
+# create vpc
+
+module "vpc" {
+  source                            = "../modules/VPC"
+  region                            = var.region
+  vpc_cidr                          = var.vpc_cidr
+  project_name                      = var.project_name
+  public_subnet_az1_cidr            = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr            = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr       = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr       = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr      = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr      = var.private_data_subnet_az2_cidr
+
+}
+
+/*
+module "nat_gateway" {
+  source                      = "../modules/nat-gateway"
+  public_subnet_az1_id        = module.vpc.public_subnet_az1_id 
+  internet_gateway            = module.vpc.internet_gateway
+  public_subnet_az2_id        = module.vpc.public_subnet_az2_id
+  vpc_id                      = module.vpc.vpc_id
+  private_app_subnet_az1_id   = module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id  = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id   = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id  = module.vpc.private_data_subnet_az2_id
+}
+*/
