6f1e4eda1f1501f1378e34f48b0fc9f332fa30dc
Tauseef Ashiq <say2ahmed@hotmail.com>
2023-04-30 13:21:35 +0100

    Added acm,alb, and ecs modules

--- 3_Tier_AWS_Network_VPC/jupiter-website-ecs/main.tf
+++ 3_Tier_AWS_Network_VPC/jupiter-website-ecs/main.tf
@@ -19,19 +19,17 @@ module "vpc" {
 
 }
 
-/*
 module "nat_gateway" {
-  source                      = "../modules/nat-gateway"
-  public_subnet_az1_id        = module.vpc.public_subnet_az1_id 
-  internet_gateway            = module.vpc.internet_gateway
-  public_subnet_az2_id        = module.vpc.public_subnet_az2_id
-  vpc_id                      = module.vpc.vpc_id
-  private_app_subnet_az1_id   = module.vpc.private_app_subnet_az1_id
-  private_data_subnet_az1_id  = module.vpc.private_data_subnet_az1_id
-  private_app_subnet_az2_id   = module.vpc.private_app_subnet_az2_id
-  private_data_subnet_az2_id  = module.vpc.private_data_subnet_az2_id
+  source                     = "../modules/nat-gateway"
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+  internet_gateway           = module.vpc.internet_gateway
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  vpc_id                     = module.vpc.vpc_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
 }
-*/
 
 module "security_groups" {
   source = "../modules/security-groups"
@@ -41,4 +39,32 @@ module "security_groups" {
 module "ecs_task_execution_role" {
   source       = "../modules/ecs-task-execution-role"
   project_name = module.vpc.project_name
+}
+
+module "acm" {
+  source            = "../modules/acm"
+  domain_name       = var.domain_name
+  alternative_names = var.alternative_names
+}
+
+module "alb" {
+  source                = "../modules/alb"
+  project_name          = module.vpc.project_name
+  alb_security_group_id = module.security_groups.alb_security_group_id
+  public_subnet_az1_id  = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id  = module.vpc.public_subnet_az2_id
+  vpc_id                = module.vpc.vpc_id
+  certificate_arn       = module.acm.certificate_arn
+}
+
+module "ecs" {
+  source                      = "../modules/ecs"
+  project_name                = module.vpc.project_name
+  ecs_task_execution_role_arn = module.ecs_task_execution_role.ecs_task_execution_role_arn
+  container_image             = var.container_image
+  region                      = module.vpc.region
+  private_app_subnet_az1_id   = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id   = module.vpc.private_app_subnet_az2_id
+  ecs_security_group_id       = module.security_groups.ecs_security_group_id
+  alb_target_group_arn        = module.alb.alb_target_group_arn
 }
